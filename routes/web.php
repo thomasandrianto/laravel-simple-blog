ef5584edce1d444f4f62c2c8b08663acfec5aba4
Thomas Andrianto <thomasandrianto1980@gmail.com>
2025-03-02 21:54:41 +0700

    updated web routes

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,28 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
-
-Route::get('/posts', function () {
-    return view('posts.index');
-})->name('posts.index');
-
-Route::get('/posts/create', function () {
-    return view('posts.create');
-})->name('posts.create');
-
-Route::get('/posts/show', function () {
-    return view('posts.show');
-});
-
-Route::get('/posts/edit', function () {
-    return view('posts.edit');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::middleware('auth')->group(function () {
+    //Route::resource('posts', PostController::class);
+
+    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+    Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+    Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+    Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 
 require __DIR__.'/auth.php';
