0a285632cbb144eaa849d477afc93cdaee95d010
Thomas Andrianto <thomasandrianto1980@gmail.com>
2025-03-02 21:29:38 +0700

    Added PostSeeder for sample posts

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Carbon;
+
+class PostSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('posts')->insert([
+            [                
+                'user_id' => 1,
+                'title' => 'Nature, Simplicity, and Zen: A Journey into Zen House Design',
+                'content' => 'The concept of Zen house design finds its roots in the Zen Buddhism philosophy, which originated in China and later spread to Japan. Zen emphasizes simplicity, mindfulness, and the beauty of natural elements, principles that have significantly influenced Japanese architecture and interior design. In its early stages, Zen design was closely tied to the minimalist aesthetics of traditional Japanese homes, known for their tatami mats, sliding doors, and serene gardens. These elements were not just functional but also deeply symbolic, reflecting a connection to nature and a focus on inner peace. The design was characterized by open, flexible spaces that encouraged contemplation and a harmonious lifestyle. As Zen philosophy gained global recognition, its influence extended beyond Japan, impacting modern architecture and interior design around the world. Contemporary Zen house design often merges traditional Japanese elements with modern materials and technology, creating spaces that are both aesthetically pleasing and spiritually calming. This fusion has led to the creation of homes that are not only visually stunning but also embody the Zen principles of harmony, simplicity, and connection with nature.',
+                'image_url' => 'storage/uploads/xQinngp76DUkavIuVUGVBJeNPAdLDMv0IFpLr4oY.png',
+                'status' => 'published',
+                'scheduled_at' => null,
+                'published_at' => '2025-02-26 12:33:00',
+                'created_at' => '2025-02-27 08:33:40',
+                'updated_at' => '2025-02-27 08:33:40',
+            ],
+            [              
+                'user_id' => 1,
+                'title' => 'The History of Java: Vernacular Residential Architecture',
+                'content' => 'It is widely believed in the history of Java that the Javanese people are descendants of Austronesian-speaking peoples. The Borobudur temple from the 9th century depicts Javanese houses that were archetypes of Austronesian dwellings with resembling pile foundations, pitched roofs, and an expanded roof bridge. The advent of the Europeans in the 16th and 17th century introduced bricks and masonry that were later adopted in house construction for the prosperous ones. Javanese traditional house forms had begun to influence the evolution of Dutch colonial architecture in Indonesia. Since the early 19th century, Dutch Indies country houses were constructed to resemble local indigenous Javanese houses as it could survive better in the intense tropical climate with heat and heavy rain. In Javanese vernacular architecture, houses are classified according to their roof configuration following the established hierarchy in the Javanese society and tradition. From the lowest to the highest, there are Omah Kampung, Limasan, and Joglo.',
+                'image_url' => 'storage/uploads/8rO4uNlcpckckfQMQGME87kr45CFo6yUNmKrbWaq.png',
+                'status' => 'published',
+                'scheduled_at' => null,
+                'published_at' => '2025-02-26 12:33:00',
+                'created_at' => '2025-02-27 08:37:04',
+                'updated_at' => '2025-02-27 08:37:04',
+            ],
+            [             
+                'user_id' => 1,
+                'title' => 'Welcome to Heritage Homes Japan',
+                'content' => 'Based in the Imperial city of Kyoto, we offer traditional Kyoto shophouses (Machiya), both unrenovated and move-in-ready, in central Kyoto. We also source, restore and relocate ancient temples, old sake breweries, farmhouses (kominkas) and other heritage buildings from all over Japan to resort areas like Niseko, Hakuba or anywhere where our clients want, including overseas. Heritage Homes Japan are fully licensed real estate agents, renovation experts, architects, contractors and project managers. We offer complete, turnkey services with a highly competent in-house team.',
+                'image_url' => 'storage/uploads/xd7p3G4uc26weeu95YBA3cQx7AN0kuZaKZbFc2AY.png',
+                'status' => 'published',
+                'scheduled_at' => null,
+                'published_at' => '2025-02-26 15:12:00',
+                'created_at' => '2025-02-27 08:42:17',
+                'updated_at' => '2025-02-27 08:42:17',
+            ],
+            [             
+                'user_id' => 1,
+                'title' => 'Tamansari Water Castle Yogyakarta',
+                'content' => 'Located near the Kraton, this place was also known as the garden for the Sultan of Yogyakarta. Tamansari was originally built for multiple purposes yet now only several buildings remain. Some of its original functions were a place to rest, to meditate, to work, to hide and to defend the Sultan’s family.  In this present day, some of its buildings have now become homes for local residents and only the mosque, resting and bathing space, and underground tunnels are accessible by tourists. With its combination of eastern and western style building, this unique escape of the royal family has its own appeal and story. The most famous place in Tamansari is the bathing and resting place of the Sultan and his Princesses named Umbul Pasiraman. Most tourists find this place interesting as there is a unique story behind its origins. The Sultan loves to go hunting during his free time and The Umbul Pasiraman was designed to appease the Sultan of that desire. Different from the Panggung Krapyak which was designed to hunt deer, the Umbul Pasiraman (which means a place to take a bath) was designed for the Princesses to take a bath and for the Sultan to relax and ‘hunt’ for a wife.',
+                'image_url' => 'storage/uploads/4axaBxE7t3YksXa9SQSF0a0KiMI6Yfw9n3r9zQLy.png',
+                'status' => 'published',
+                'scheduled_at' => null,
+                'published_at' => '2025-02-26 15:12:00',
+                'created_at' => '2025-02-27 08:42:17',
+                'updated_at' => '2025-02-27 08:42:17',
+            ],
+            [             
+                'user_id' => 1,
+                'title' => 'Japanese Buddhist Architecture',
+                'content' => 'Japanese Buddhist Architecture mainly includes the architecture of Buddhist temples which was influenced by the architectural styles from China. Earlier, the attempts were to make the Buddhist architecture as original as it was looked in China but gradually the buildings were localized due to the problems posed by local weather and Japanese tastes. The Buddhism and the Buddhist architecture were literally imported from China via Korea in the 6th century. As the Buddhism was introduced in Japan, the Buddhist temples were started to build in the country but due to the hostile behavior of supporter of the local kami, the buildings were no longer stand by itself and there are no written records of the architectural styles of that period. But later the Buddhism got its support from the Prince Shotoku. He ordered the construction of Buddhist temple, Shitennoji in Osaka (593) and Horyu-ji near his palace in Ikaruga (603). During this period, the temple layout was strictly prescribed and followed. This act helped to maintain the architectural style uniform. In this period the main gate was constructed facing south and the most sacred area surrounded by a semi-enclosed roofed corridor accessible through a middle gate. The temple complex also contains the main hall with Buddha statue, and pagoda which houses sacred objects. The other structures include a lecture hall, a belfry, a sutra repository, priests and monks quarters and bathhouse. Nara Period observed quite different architectural development. The temple structures, such as pagodas and main halls, had increased significantly in size. The placement of the pagoda moved to a more peripheral location and the roof bracketing system increased in complexity as roofs grew larger and heavier. In the 8th century, Kami worship and Buddhism was reconciled and thus shrine-temples were founded to support both groups. This coexistence of Buddhism and Kami worship continued until the Kami and Buddhas Separation Order of 1868.',
+                'image_url' => 'storage/uploads/021OP2po8wUplyI3PSDeKFiqVWve2jQn3OWubMv1.png',
+                'status' => 'scheduled',
+                'scheduled_at' => '2025-03-05 13:14:00',
+                'published_at' => '2025-02-26 15:12:00',
+                'created_at' => '2025-02-27 08:42:17',
+                'updated_at' => '2025-02-27 08:42:17',
+            ],
+            [             
+                'user_id' => 1,
+                'title' => 'The Charm and Philosophy of Japanese Gardens',
+                'content' => 'Today, as Japanese culture is said to be losing its traditional character, the Japanese garden is probably one of the most important things. Japanese gardens embody more than mere beauty; they are important in understanding Japanese culture, such as coexistence with nature and spiritual peace. What is a Japanese garden? Japanese gardens specialize in imitating nature and using stones, water, and plants to depict the landscape. These elements are planned to encourage reflection and serenity in the viewer. The rocks represent eternity and strength, the water represents purity and the beauty of change, and the plants represent the changing seasons. One of the most distinctive elements is the intentional placement of all these elements while mimicking perfect nature. ',
+                'image_url' => 'storage/uploads/1ycFoLkwOVNopJKCpaLqcGDfmL7W58eZLP5nXDja.png',
+                'status' => 'draft',
+                'scheduled_at' => null,
+                'published_at' => null,
+                'created_at' => '2025-02-27 16:07:36',
+                'updated_at' => '2025-02-27 16:07:36',
+            ]
+        ]);
+    }
+}
